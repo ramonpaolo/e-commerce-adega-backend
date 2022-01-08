37722365bd4b419c522b628fbf28b1f4c080248b
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-01-08 10:22:38 -0300

    Added route to add and remove product on cart

--- src/routes/cart.ts
+++ src/routes/cart.ts
@@ -1,11 +1,42 @@
-import express from "express"
+import express from "express";
 
-import {AuthValidationController} from "../controllers/authController"
+//---- Controllers
+import { CartControllerShow } from "../controllers/cartController";
+import { UserControllerUpdate } from "../controllers/userController";
 
-const app = express()
+const app = express();
 
-const authValidationController = new AuthValidationController();
+app.get("/cart", async (req, res) =>
+  res.send(await new CartControllerShow().showProductsOnCart(req, res))
+);
 
-app.get("/cart", async (req, res) => res.send({"cart": [{name: "Biscoito"}]}))
+app.put("/cart/:idProduct", async (req, res) => {
+  let ok: boolean = await new UserControllerUpdate().addProductOnCart(req, res);
+  if (ok)
+    return res.status(200).send({
+      status: "success",
+      message: `product ${req.params.idProduct} was added on cart`,
+      codeProduct: req.params.idProduct,
+    });
+  return res.status(404).send({
+    status: "error",
+  });
+});
 
-export default app
+app.delete("/cart/:idProduct", async (req, res) => {
+  let ok: boolean = await new UserControllerUpdate().removeProductOnCart(
+    req,
+    res
+  );
+  if (ok)
+    return res.status(200).send({
+      status: "success",
+      message: `product ${req.params.idProduct} was removed on cart`,
+      codeProduct: req.params.idProduct,
+    });
+  return res.status(404).send({
+    status: "error",
+  });
+});
+
+export default app;
