4e2af79d71da69e095863189808db8f384854fcc
Ramon Paolo Maran <ramonpaolomaran12@gmail.com>
2022-01-08 10:21:53 -0300

    Added product/remove on cart user

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,55 @@
-class UserController{
-    
+import { Request, Response } from "express";
+
+//---- Models
+import UserModel from "../models/userModel";
+
+// class UserControllerShow {
+//   public async getName(req: Request, res: Response) {}
+//   public async getEmail(req: Request, res: Response) {}
+//   public async getImage(req: Request, res: Response) {}
+//   public async getComments(req: Request, res: Response) {}
+// }
+
+class UserControllerUpdate {
+  public async updateName(req: Request, res: Response) {}
+  public async updateEmail(req: Request, res: Response) {}
+  public async updatePassword(req: Request, res: Response) {}
+  public async updateImage(req: Request, res: Response) {}
+  public async excludeComment(req: Request, res: Response) {}
+
+  public async addProductOnCart(req: Request, res: Response): Promise<boolean> {
+    try {
+      await UserModel.updateOne(
+        { email: res.locals.user.email },
+        {
+          $push: {
+            cart: req.params.idProduct,
+          },
+        }
+      );
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
+  public async removeProductOnCart(
+    req: Request,
+    res: Response
+  ): Promise<boolean> {
+    try {
+      await UserModel.updateOne(
+        { email: res.locals.user.email },
+        {
+          $pull: {
+            cart: req.params.idProduct,
+          },
+        }
+      );
+      return true;
+    } catch (e) {
+      return false;
+    }
+  }
 }
 
-class UserControllerUpdate{
-
-}
+export { UserControllerUpdate };
